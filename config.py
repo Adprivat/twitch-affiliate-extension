0c36a6f0cbe753ab78ffff235e1b2a78de92891a
ASchultz <131669459+Adprivat@users.noreply.github.com>
2025-02-11 11:56:58 +0100

    DB Update

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 # Standard-MongoDB-Verbindungszeichenfolge mit URL-codiertem Passwort.
 # Ersetze 'my_affiliate_db' ggf. durch den Namen deiner Datenbank.
-MONGO_URI = os.environ.get(
-    'MONGO_URI',
-    "mongodb+srv://Testbenutzer:Kira2018%3F%3F%3F@testcluster1.6xbqa.mongodb.net/my_affiliate_db?retryWrites=true&w=majority"
-)
+MONGO_URI = os.environ.get('MONGO_URI', "").strip()
+if not MONGO_URI:
+    MONGO_URI = "mongodb+srv://Testbenutzer:Kira2018%3F%3F%3F@testcluster1.6xbqa.mongodb.net/my_affiliate_db?retryWrites=true&w=majority"
+
