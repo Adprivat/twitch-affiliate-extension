9b562e1be8e0669a1af1c81b107df47c875654d4
ASchultz <131669459+Adprivat@users.noreply.github.com>
2025-02-12 10:33:43 +0100

    Testvorbereitung

--- app.py
+++ app.py
@@ -123,5 +123,5 @@ def remove_x_frame_options(response):
     return response
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=True, use_reloader=False)
